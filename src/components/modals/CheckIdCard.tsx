049f1d29d8dd0d4531e999512b27b2f03cd3bd6a
DEV SRIWARARAK <devsriwararak.work@gmail.com>
2025-06-22 22:56:56 +0700

    change check id card

--- src/components/modals/CheckIdCard.tsx
+++ src/components/modals/CheckIdCard.tsx
@@ -11,6 +11,7 @@ import { useRouter } from 'next/navigation'
 import { MemberDataType } from '@/app/admin/member/page'
 import moment from 'moment'
 import 'moment/locale/th'
+import DatePicker from '../form/date-picker'
 moment.locale('th')
 
 interface Props {
@@ -22,21 +23,25 @@ interface Props {
 const CheckIdCard = ({ isOpen, closeModal, type }: Props) => {
 
     // Systems
-    const { control, handleSubmit } = useForm<MemberDataType>({ defaultValues: { idCard: "", idCardType : 1 }, })
+    const { control, handleSubmit } = useForm<MemberDataType>({ defaultValues: { idCard: "", idCardType: 1, dateOfTraining: "" }, })
     const router = useRouter()
 
     // State 
     const [idCardSwitch, setIdCardSwitch] = useState<number>(1)
+    const [dateOfTrainingStatus, setDateOfTrainingStatus] = useState<boolean>(false)
+    const [idCard, setIdCard] = useState<string>("")
+    const [dateOfTraining, setDateOfTraining] = useState<string>("")
 
 
-    const onSubmit = async (data: { idCard: string, idCardType :number }) => {
+    const onSubmit = async (data: { idCard: string, idCardType: number }) => {
 
         const payload = data.idCard
         if (!payload) return toast.error('ส่งข้อมูลไม่ครบ')
+        setIdCard(payload)
 
         try {
             const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/member/checkIdCard`, { idCard: payload })
-
+            console.log(res);
             if (res.status === 200 || res.status === 201) {
                 console.log(res.data);
                 const idCard = res.data.idCard
@@ -58,8 +63,8 @@ const CheckIdCard = ({ isOpen, closeModal, type }: Props) => {
                         router.replace(`/member/questions/${idCard}`)
                     }
                 } else {
-                    toast.error('ใบเซอร์ของคุณหมดอายุแล้ว กรุณาแจ้ง ADMIN แก้ไขข้อมูลลงทะเบียนให้')
-                    return
+                    toast.error('เกิดข้อผิดพลาด')
+                    setDateOfTrainingStatus(true)
                 }
 
             }
@@ -84,6 +89,21 @@ const CheckIdCard = ({ isOpen, closeModal, type }: Props) => {
         });
         toast.error(`กรุณากรอกข้อมูลให้ครบถ้วน: \n- ${fieldNames.join("\n- ")}`)
     };
+
+    const handleAddDateOfTraining = async () => {
+        try {
+            if (!idCard) return toast.error(`ไม่พบรหัสบัตรประชาชน`)
+                console.log(idCardSwitch);
+                
+            const res = await axios.put(`${process.env.NEXT_PUBLIC_API_URL}/api/member/user/update/${idCard}`, { dateOfTraining })
+            if (res.status === 200) {
+                await onSubmit({ idCard: res.data.idCard, idCardType: idCardSwitch })
+            }
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
 
 
 
@@ -123,7 +143,7 @@ const CheckIdCard = ({ isOpen, closeModal, type }: Props) => {
                                     )}
                                 /> */}
 
-                                {idCardSwitch === 1 && (
+                                {idCardSwitch === 1 && !dateOfTrainingStatus && (
                                     <>
                                         <Label>เลขบัตรประชาชน 13 หลัก</Label>
                                         <Controller
@@ -150,7 +170,7 @@ const CheckIdCard = ({ isOpen, closeModal, type }: Props) => {
                                         />
                                     </>
                                 )}
-                                {idCardSwitch === 2 && (
+                                {idCardSwitch === 2 && !dateOfTrainingStatus && (
                                     <>
                                         <Label>Passport No.</Label>
                                         <Controller
@@ -175,49 +195,74 @@ const CheckIdCard = ({ isOpen, closeModal, type }: Props) => {
                                 )}
                             </div>
 
-                            <div className='flex flex-row gap-3 items-center mt-2 justify-start'>
+                            {!dateOfTrainingStatus && (
+                                <div className='flex flex-row gap-3 items-center mt-2 justify-start'>
+                                    <div>
+                                        <label className="text-sm font-medium text-gray-700">เลือกสัญชาติ</label>
+                                        <Controller
+                                            name="idCardType"
+                                            control={control}
+                                            render={({ field }) => (
+                                                <div className="flex items-center space-x-6 mt-2">
+                                                    {/* ไทย */}
+                                                    <label className="flex items-center space-x-2">
+                                                        <input
+                                                            type="radio"
+                                                            value={1}
+                                                            checked={field.value == 1}
+                                                            onChange={() => { field.onChange(1); setIdCardSwitch(1) }}
+                                                            className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300"
+                                                        />
+                                                        <span className="text-gray-700">ไทย</span>
+                                                    </label>
 
-                                <div>
-                                    <label className="text-sm font-medium text-gray-700">เลือกสัญชาติ</label>
-
-                                    <Controller
-                                        name="idCardType"
-                                        control={control}
-                                        render={({ field }) => (
-                                            <div className="flex items-center space-x-6 mt-2">
-                                                {/* ไทย */}
-                                                <label className="flex items-center space-x-2">
-                                                    <input
-                                                        type="radio"
-                                                        value={1}
-                                                        checked={field.value == 1}
-                                                        onChange={() => { field.onChange(1); setIdCardSwitch(1) }}
-                                                        className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300"
-                                                    />
-                                                    <span className="text-gray-700">ไทย</span>
-                                                </label>
-
-                                                {/* ต่างชาติ */}
-                                                <label className="flex items-center space-x-2">
-                                                    <input
-                                                        type="radio"
-                                                        value={2}
-                                                        checked={field.value == 2}
-                                                        onChange={() => { field.onChange(2); setIdCardSwitch(2) }}
-                                                        className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300"
-                                                    />
-                                                    <span className="text-gray-700">ต่างชาติ</span>
-                                                </label>
-                                            </div>
-                                        )}
-                                    />
+                                                    {/* ต่างชาติ */}
+                                                    <label className="flex items-center space-x-2">
+                                                        <input
+                                                            type="radio"
+                                                            value={2}
+                                                            checked={field.value == 2}
+                                                            onChange={() => { field.onChange(2); setIdCardSwitch(2) }}
+                                                            className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300"
+                                                        />
+                                                        <span className="text-gray-700">ต่างชาติ</span>
+                                                    </label>
+                                                </div>
+                                            )}
+                                        />
+                                    </div>
                                 </div>
-                            </div>
+                            )}
+
+                            {dateOfTrainingStatus && (
+                                <div className='w-full'>
+                                    <p className='text-red-600'>ใบเซอร์หมดอายุ กรุณาระบุวันที่ใหม่ *</p>
+                                    <div className='mt-3'>
+                                        <DatePicker
+                                            id="dateOfTraining"
+                                            label="เลือกวันที่เข้าอบรม"
+                                            placeholder="เลือกวันที่"
+                                            onChange={(e) => {
+                                                if (e && e[0]) {
+                                                    setDateOfTraining(e[0].toISOString())
+                                                }
+                                            }}
+                                        />
+                                    </div>
+                                </div>
+                            )}
 
                             <div className="flex items-center gap-3 px-2 mt-6 lg:justify-center ">
-                                <Button size="sm" type="submit" className='w-full' >
-                                    ตรวจสอบ
-                                </Button>
+                                {dateOfTrainingStatus ? (
+                                    <Button size="sm" type="button" className='w-full' onClick={handleAddDateOfTraining} >
+                                        บันทึก และ เข้าระบบ
+                                    </Button>
+                                ) : (
+                                    <Button size="sm" type="submit" className='w-full' >
+                                        ตรวจสอบ
+                                    </Button>
+                                )}
+
                             </div>
 
                         </form>
