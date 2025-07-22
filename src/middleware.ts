d50f58f01f0e40b21351d414133aacf46ab13b0a
DEV SRIWARARAK <devsriwararak.work@gmail.com>
2025-07-23 02:17:15 +0700

    change new database to sql server

--- src/middleware.ts
+++ src/middleware.ts
@@ -108,7 +108,7 @@
 // };
 
 
-// middleware.ts
+// middleware.ts ...
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
